692496115f18066b2913a7c075932cf53d66f794
Shayaan Abdullah <sabdullah@berkeley.edu>
2016-09-14 22:46:06 -0700

    Add Training Tools

---
+++ training_tools.py
@@ -0,0 +1,260 @@
+from __future__ import division
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import png
+import csv
+from scipy.integrate import ode
+
+
+class grid:
+
+	def __init__(self,file_name):
+		self.grid_size = 64
+		self.grid = np.zeros((self.grid_size,self.grid_size))
+		self.save_name = "Training_Images/" + file_name + ".png"	
+
+	def draw_figure(self,pos_array):
+	    #this function should take the end effector position and draw on a 64 by 64 grid
+	    for pos in pos_array:
+	        self.grid[pos[0],pos[1]] = 255
+	    return self.grid
+
+	def save_image(self):
+	    png.from_array(self.grid.tolist(),'L').save(self.save_name)
+	    
+	def show_image(self):
+	    img = plt.imread(self.save_name)
+	    return plt.imshow(img,cmap ='Greys_r')
+
+
+class shape_maker:
+
+	def __init__(self):
+		self.std = 6
+		self.mu = 32
+
+	def draw_line(self,start_point,end_point,step_size,tol):
+		"""
+		Args: A tuple for start_point and end_point,tuple of the form (row,column)
+		Returns: Array of tuples each entry corresponding to the position of the line
+		"""
+		#first unpack the tuples 
+		end_point_x,end_point_y = end_point
+		start_point_x,start_point_y = start_point
+		#initialize an output array
+		output_array = [start_point]
+		#initialize current point
+		current_point_x,current_point_y = start_point
+		#first deal with situation where startpoint and endpoint lie in the same row i.e. line is horizontal
+		if end_point_x == start_point_x:
+			#if end point is to the right of the start point
+			if end_point_y > start_point_y:
+			    output_array_y = range(start_point_y,end_point_y + 1)
+			    output_array_x = [start_point_x] * len(output_array_y)
+			else:
+			    output_array_y = range(end_point_y,start_point_y + 1)
+			    output_array_x = [start_point_x] * len(output_array_y)
+			return zip(output_array_x,output_array_y)
+
+		#if on the other hand this special case does not come into consideration determine the gradient of the line
+		m = (end_point_y - start_point_y)/(end_point_x - start_point_x)
+		#find the y intercept of the line
+		c = start_point_y - m*start_point_x
+		while abs(current_point_x - end_point_x) > tol:
+			#increment the row position of the point and calculate the relevant column position up until the end point is reached
+			current_point_y = np.round((m*current_point_x + c))
+			if start_point_x < end_point_x:
+			    current_point_x += step_size
+			else:
+			    current_point_x -= step_size
+			#since we only want whole numbers we round the current point to get the position of the marker but only
+			#include the tuple in our output array if the new point is different than the previous one. 
+			if np.round(current_point_x) != output_array[-1][0] or np.round(current_point_y) != output_array[-1][1]:
+			    output_array.append((np.round(current_point_x), np.round(current_point_y)))
+		return output_array
+
+
+	def get_vertices(self,shape):
+
+		"""
+		we require a function that can take the abstract specification of a geometric shape and translate
+		the above to a lower level abstraction in the case of geometric shapes this is just the start point
+		and end point of each line segment
+		Args: shape - a string specifying the shape to be drawn
+		Returns: A list of tuples corresponding to the vertices of the shapes
+		"""
+		if shape == "Square":
+			d = np.floor(self.sample_truncated_normal(9,10,30,1))[0]
+			vertices = [(-d,0),(0,d),(d,0),(0,-d)]
+		if shape == "Triangle":
+			del_1 = np.floor(self.sample_truncated_normal(10,10,40,2))
+			del_2 = np.floor(self.sample_truncated_normal(10,10,40,2))
+			del_3 = -(del_1 + del_2)
+			vertices = [del_1,del_2,del_3]
+		if shape == "Rectangle":
+			d = np.floor(self.sample_truncated_normal(9,10,30,2))
+			vertices = [(-d[0],0),(0,d[1]),(d[0],0),(0,-d[1])]
+
+		return vertices
+
+
+	def sample_truncated_normal(self,mu,std,threshold,dim):
+		"""
+		This is utilized by the function that generates vertices given the shape of needed 
+		"""
+		sample = std*np.random.randn(dim) + mu
+		if np.sum(np.logical_or(abs(sample) > threshold , abs(sample) < 5)) > 0:
+			return self.sample_truncated_normal(mu,std,threshold,dim)
+		else:
+			return sample
+
+
+	def get_points(self,shape):
+		"""
+		This is the main function and is responsible for calling on the others to generate
+		an array of tuples with each tuple denoting a pixel to be filled in the grid
+		"""
+		pos_array = []
+		#generate a random start point for the grid
+		start_point = np.round(self.std*np.random.randn(2) + self.mu)
+		#then get the vertices of the shape required
+		vertices = self.get_vertices(shape)
+		if self.check(vertices,start_point):
+			for point in vertices:
+				end_point = (int(start_point[0] + point[0]),int(start_point[1] + point[1]))
+				pos_array.extend(self.draw_line((start_point[0],start_point[1]),end_point,0.001,0.001))
+				start_point = end_point
+			return pos_array
+
+
+	def check(self,control_vector,start_point):
+		prev_point = start_point
+		vector_list = []
+		angle = 90
+		j = 0
+		for control in control_vector:
+			current_point = (int(start_point[0] + control[0]),int(start_point[1] + control[1]))
+			vector = np.subtract(current_point,start_point)
+			vector_list.append(vector)
+			if len(vector_list) > 1:
+				angle = np.arccos(np.dot(vector_list[j],vector_list[j+1])/(np.linalg.norm(vector_list[j+1])*np.linalg.norm(vector_list[j])))* 360/(2*np.pi)
+				j += 1
+			if abs(current_point[0]) > 63 or abs(current_point[1]) > 63 or current_point[0] < 0 or current_point[1] < 0 or abs(angle) < 25 or abs(angle) > 155 :
+				return False
+		start_point = current_point
+		return True
+
+
+	def gen_shapes(self,shape_str,num):
+		#initialize a list to hold the point arrays
+		pos_list = [0] * num
+		for i in range(num):
+			#initialize a grid object
+			my_grid = grid(shape_str + str(num))
+			#generate the point
+			points_array = self.get_points(shape_str)
+			#add points to list
+			pos_list[i] = points_array
+			#pass the points to the grid
+			my_grid.draw_figure(points_array)
+			#now save the figure
+			my_grid.save_image()
+		return pos_list
+
+class two_link_arm:
+
+	def __init__(self):
+		self.ic = [0,0,0,0]
+		self.t_end = 5
+
+
+	def forward_dynamics(self,Torques):
+		""" Args: Torques - a 2d array of forces and torques of size [num of forces * length of time]
+		Returns: Accel - a 2d array of accelerations of size [num of degrees of motion * length of time]
+		"""
+		#using Newtonian dyanmics the Torque at each link is related to the angle at that link by T = I*theta''
+		#use the torques to find the thetas generated over a period of time
+		#there are four systems of equations that need to be numerically integrated in order to obtain the correct thetas
+		#initialize a states 
+		I = 1
+		def f(t,y,arg1,arg2):
+			y_prime = [y[1],arg1/I,y[3],arg2/I]
+			return y_prime
+		#set initial displacements and angular velocities to be zero
+		r = ode(f).set_integrator('lsoda')
+		i = 0
+		t0 = 0
+		#get the number of timesteps that are used to generate the torques
+		_,timesteps = np.shape(Torques)
+		#initialize a states array
+		states = np.zeros((4,timesteps))
+		dt = self.t_end / timesteps
+		r.set_initial_value(self.ic, t0).set_f_params(Torques[0,i],Torques[1,i])
+		while r.successful() and r.t < (self.t_end - dt):
+			r.set_f_params(Torques[0,i],Torques[1,i])
+			temp =  r.integrate(r.t + dt)
+			#use the above to integrate the
+			states[:,i] = temp
+			i += 1
+		return states
+
+
+	def forward_kinematics(self,state):
+		""" Args: State -  a 2d array of positions of size [num of degrees of motion * length of time]
+		    Returns: Effector_Position - a list of tuples consisting of (x,y) position of arm effector and with number of entries 
+		                             equal to the length of time being considered.
+		"""
+		#use the general two link arm with theta1 and theta2
+		l = 1
+		theta_1 = state[0,:]
+		theta_2 = state[2,:]
+		x = l*(np.cos(theta_1) + np.cos(theta_1 + theta_2))
+		y = l*(np.sin(theta_1) + np.sin(theta_1 + theta_2))
+		Effector_position = zip(x,y)
+		return Effector_position
+
+	def inverse_kinematics(self,Effector_position):
+	    """ Args: Effector_Position - a list of tuples consisting of (x,y) position of arm effector and with number of entries 
+	                                 equal to the length of time being considered.
+	        Returns: State -  a 2d array of positions of size [num of degrees of motion * length of time]
+	    """
+	    #given x-y cartesian coordinates is it possible to obtain the values of theta and phi that correspond to the end effector position
+	    l = 1
+	    theta_1 = [0] * len(Effector_position)
+	    theta_2 = [0] * len(Effector_position)
+	    for i,pos in enumerate(Effector_position):
+	        x = pos[0]
+	        y = pos[1]
+	        theta_2[i] = np.arccos((x**2 + y**2 - 2*(l**2))/(2*(l**2)))
+	        k1 = l*(1 + np.cos(theta_2[i]))
+	        k2 = l*np.sin(theta_2[i])
+	        theta_1[i] = np.arctan(y/x) - np.arctan(k2/k1)
+	    #get the value of the 
+	    states = np.vstack((theta_1,self.forward_difference(theta_1),theta_2,self.forward_difference(theta_2)))
+	    return states
+
+	def inverse_dynamics(self,states):
+	    """ Args : Accel -  a 2d array of accelerations of size [num of degrees of motion * length of time]
+	        Returns: Forces - a 2d array of forces and torques of size [num of forces * length of time]
+	    """
+	    
+	    #in case of 2 link arm there are only two forces to worry about call them T_phi and T_theta then using newton's laws
+	    #one may obtain the inverse dynamics
+	    # Assuming that the moment of inertia for the two bars is the same and that we are computing the acceleration at the bottom
+	    #of each bar we may obtain the forces as follows
+	    #one has to use the states signal and use it to get the torques
+	    torques_1 = self.forward_difference(states[1,:])
+	    torques_2 = self.forward_difference(states[3,:])
+	    Torques = np.vstack((torques_1,torques_2))
+	    return Torques
+
+	def forward_difference(self,ndarray):
+		dt = self.t_end /len(ndarray)
+		#zero pad the ndarray
+		ndarray = np.concatenate(([0],ndarray))
+		derivative = np.diff(ndarray) / dt
+		return derivative
+
+
+
--- variable_seq2seq.py
+++ variable_seq2seq.py
@@ -3,54 +3,46 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-#first thing to do is to define some global variables
 DECODER_MAX_LENGTH = 20
+ENCODER_MAX_LENGTH = 15
 BATCH_SIZE = 15
-HIDDEN_UNITS = 1 
-INPUT_FEATURES = 1
+HIDDEN_UNITS = INPUT_FEATURES = 1
 OUTPUT_FEATURES = 1
+NUM_BATCHES = 10
 LEARNING_RATE = 1e-3
-FC_UNITS = DECODER_MAX_LENGTH*BATCH_SIZE*HIDDEN_UNITS
 
 
-#first define a function that constructs the decoder
+
 class variable_lstm:
 
 	def __init__(self):
-		self.hidden_units = HIDDEN_UNITS
-		self.batch_size = BATCH_SIZE
-		self.input_features = INPUT_FEATURES
-		self.learning_rate = LEARNING_RATE
-		self.encoder_lstm = tf.nn.rnn_cell.BasicLSTMCell(HIDDEN_UNITS)
-		#initialize a tensor for the curren time step
-		self.cur_tstep =tf.constant(1,shape = [1])
-				
-		#instead of using a boolean array just define a termination point in the sequence
-		self.termination_tstep = tf.placeholder(tf.int32,shape = [1],name = "test")
-		#define input place holder for the variable length input
-		self.x_input = tf.placeholder(tf.float32, shape = [BATCH_SIZE,INPUT_FEATURES,None])
-		#self.W_fc = tf.Variable(tf.truncated_normal(shape = [FC_UNITS,DECODER_MAX_LENGTH*OUTPUT_FEATURES*BATCH_SIZE]))
-		#self.b_fc = tf.Variable(tf.constant(0.0,shape = [DECODER_MAX_LENGTH*OUTPUT_FEATURES*BATCH_SIZE]))
-		self.op_dict = {}
-	
+		#initialize some variables
+		self.cur_tstep = tf.constant(1,shape = [1])
+		#specify the termination tstep 
+		self.termination_tstep = tf.placeholder(tf.int32,shape = [1])
+		#initialize LSTM cell
+		self.lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(HIDDEN_UNITS,state_is_tuple = True)
+		#define the input of shape [Batch_size,Num of hidden units,outputs]
+		self.x = tf.placeholder(tf.float32,shape = [BATCH_SIZE,INPUT_FEATURES,ENCODER_MAX_LENGTH])
+
+
 	def construct_decoder(self,old_state):
 		"""
-		Constructs the operations for the decoder sub-graph which is initialized by the last state of the encoder.
-		The subgraph hidden state serves as the output at each timestep.
-		Args: old_state - Tensor of shape [Batch_Size,Hidden_Units * 2] 
-			  last_output - Tensor of shape [Batch_Size,Hidden_Units]
-		Returns: decoder_output - List of Tensors of shape [Batch_Size,Features], length of Decoder is DECODER_MAX_LENGTH
+		Takes the cell and hidden state from the encoder and feeds it to a decoder in order to get
+		an output equal to the decoder length
+		input : old_state - tuple with first entry corresponding to the cell state and second the hidden state
+							each entry has shape [Batch_Size, Number of Units]
+
+		output : decoder_output - A list of outputs at each cell of the LSTM. The shape is [DECODER_MAX_LENGTH,BATCH_SIZE,HIDDEN_UNITS]
 		"""
+
 		with tf.variable_scope("Decoder_RNN") as scope:
 			#initialize the decoder lstm cell
-			decoder_lstm = tf.nn.rnn_cell.BasicLSTMCell(HIDDEN_UNITS)
+			decoder_lstm = tf.nn.rnn_cell.BasicLSTMCell(HIDDEN_UNITS, state_is_tuple = True)
 			#initialize a list for decoder output
 			decoder_output = [0] * DECODER_MAX_LENGTH
-			#get the last output by splitting the state
-			print old_state
-			last_output,_ = tf.split(1,2,old_state)
 			#now unroll the decoder lstm plugging in the previous input at each time step
+			last_output = old_state[0]
 			for tstep in range(DECODER_MAX_LENGTH):
 				cur_output,new_state = decoder_lstm(last_output,old_state,scope = "Cell" + str(tstep))
 				#append the cur_output to the decoder_output array
@@ -62,118 +54,68 @@ class variable_lstm:
 		return decoder_output
 
 
-	def unroll_one_tstep(self,x,cur_tstep,termination_tstep,cell,state):
-		"""
-		Takes RNN cell its previous state and current input and unrolls it for one time step
-		Args: cell - a Tensorflow cell to unroll
-			  state - A 2d tensor of shape [BATCH_SIZE,HIDDEN_UNITS*2]
-			  input - A 2d tensor of shape [BATCH_SIZE,FEATURES]
-		Returns: output - A 2d tensor of shape [BATCH_SIZE,HIDDEN_UNITS]
-				 new_state - A 2d tensor of shape [BATCH_SIZE,HIDDEN_UNITS*2]
-		"""
-		#take the input x and slice out the x_input that belongs to the right tstep
-		#first define the beginning index
-		index_start = tf.concat(0,[tf.constant([0,0]),cur_tstep])
-		slice_size = tf.constant([-1,-1,1])
-		x_slice = tf.slice(x,index_start,slice_size)
-		#now reshape x_slice into a 2d tensor
-		x_reshape = tf.reshape(x_slice,shape = [BATCH_SIZE,INPUT_FEATURES])
-		output,state = cell(x_reshape,state,scope = "Cell" + str(cur_tstep))
-		return state
+	def body(self,tstep,old_m_state,old_c_state):
+		new_tstep = tf.add(tstep,tf.constant(1, shape = [1]))
+		#concatentate the new tstep with the
+		index = tf.concat(0,[tf.constant([0,0]),new_tstep])
+		size = tf.constant([-1,-1,1])
+		x_sliced = tf.slice(self.x,index,size)
+		x_reshape = tf.reshape(x_sliced,shape = [BATCH_SIZE,INPUT_FEATURES])
+		#now feed this into lstm
+		output,state = self.lstm_cell(x_reshape,(old_m_state,old_c_state))
+		return new_tstep,state[0],state[1]
 
-	#initialize the encoder lstm cell
+	
+	def cond(self,tstep,old_m_state,old_c_state):
+		return tf.less(tstep,self.termination_tstep)[0]
 
-	def cond(self,cur_tstep,termination_tstep,state,x_input):
-		"""
-		Takes the termination step and returns a boolean specifying if the condition has been met
-		Args: termination_tstep - An integer tensor specifying the end of the input variable length sequence
-			  cur_tstep - The current tstep in the position
-		Returns: An integer Boolean tensor specifying if the end of the while loop has been reached
-		"""
-		boolean = tf.less(cur_tstep ,termination_tstep)[0]
-		return boolean
 
-	def body(self,cur_tstep,termination_tstep,state,x_input):
+	def build_graph(self):
 		"""
-		This function takes the loop variables and updates them for every timestep
-		Args: 
-		Returns: The loop variables which are updated 
+		specifies the operations that build the computational graph
+		output : op_list - a list of tensors corresponding to operations that may be required in another function
+				 sess - a tensorflow object required to evaluate operations that constitute the graph
 		"""
-		#first call on unroll_one_tstep in order to get the new state
-		state = self.unroll_one_tstep(x_input,cur_tstep,termination_tstep,self.encoder_lstm,state)
-		#termination_tstep remains fixed so does the input_x and so does lstm_cell
-		cur_tstep = tf.add(cur_tstep ,tf.constant(1,shape = [1]))
-		return cur_tstep,termination_tstep,state,x_input
-
-	#now construct the encoder part of the RNN
-	def construct_graph(self,state):
-		"""
-		Constructs tensorflow graph using the weights of
-		"""
-		#define the loop variable list that will be used by tf.while_loop
-		loop_variable = [self.cur_tstep,self.termination_tstep,state,self.x_input]
-		#use tf.while_loop to get the required output
-		r = tf.while_loop(lambda  cur_tstep,termination_tstep,state,x_input : self.cond(cur_tstep,termination_tstep,state,x_input),
-			lambda cur_tstep,termination_tstep,state,x_input : self.body(cur_tstep,termination_tstep,state,x_input), loop_variable )
-		self.op_dict['fc'] = tf.pack(self.construct_decoder(r[2]))
-		print self.op_dict['fc']
-		# #reshape the fully connected layer to get a 2d array
-		#self.op_dict['fc_reshape'] = tf.reshape(self.op_dict['fc'],[-1,FC_UNITS])
-		# #define a weight variable to transform the output to what is required
-		
-		# #use the above to obtain the y that you want
-		# y_2d = tf.nn.tanh(tf.matmul(self.op_dict['fc_reshape'],self.W_fc))
-		# #now reshape to obtain the correctly shaped y
-		self.op_dict['y'] = tf.reshape(self.op_dict['fc'],shape = [BATCH_SIZE,OUTPUT_FEATURES,DECODER_MAX_LENGTH])
-		# #define an input for the label 
-		self.op_dict['y_'] = tf.placeholder(tf.float32,shape = [BATCH_SIZE,OUTPUT_FEATURES,DECODER_MAX_LENGTH])
-		# #now define a loss node and a training node using
-		self.op_dict['meansq'] = tf.reduce_mean(tf.square(tf.sub(self.op_dict['y_'],self.op_dict['y'])))
-		# #now define a training node using the above
-		self.op_dict['train_op'] = tf.train.AdamOptimizer(self.learning_rate).minimize(self.op_dict['meansq'])
+		#specify a placeholder 
+		y_ = tf.placeholder(tf.float32,shape = [BATCH_SIZE,HIDDEN_UNITS,DECODER_MAX_LENGTH])
+		#initialize the state tuple
+		state_tuple = self.lstm_cell.zero_state(BATCH_SIZE,tf.float32)
+		loop_var = [self.cur_tstep,state_tuple[0],state_tuple[1]]
+		#use the above to construct the while loop 
+		r = tf.while_loop(lambda tstep,old_m_state,old_c_state : self.cond(tstep,old_m_state,old_c_state),lambda tstep,old_m_state,old_c_state : self.body(tstep,old_m_state,old_c_state), loop_var)
+		#use the encoder output to initialize the decoder
+		decoder_output = self.construct_decoder((r[1],r[2]))
+		#reshape the decoder output such that it may be used to compute a loss
+		y = tf.reshape(decoder_output, shape = [BATCH_SIZE,HIDDEN_UNITS,DECODER_MAX_LENGTH])
+		#compute a loss
+		meansq = tf.reduce_mean(tf.square(tf.sub(y,y_)))
+		#use the loss to define a training operation 
+		train_op = tf.train.AdamOptimizer(LEARNING_RATE).minimize(meansq)
+		#define a list of operations that would be useful 
+		op_list = [y_,y,meansq,train_op]
+		#initialize graph
 		sess = tf.Session()
-		#sess.run(tf.initialize_all_variables())
-		return sess
+		#initialize variables
+		sess.run(tf.initialize_all_variables())
+		return sess,op_list
 
-	def train_graph(self,Epoch,x_list,y_list,termination_tstep_list,sess):
-		"""
-		Treat the x_list and y_list to be lists of tensors of shape =[batch_size,features,sequence_length]
-		returns a session object that can be used to evaluate the graph for a certain input
 
-		"""
-		#first get a sessions object and initialize all the variables
-		#initialize a loss array
+	def train_graph(self,sess,op_list,x_list,y_list,termination_tstep_list):
 		loss_array = [0] * len(x_list)
-		#now run the train operation for each batch of inputs
 		for index in range(Epoch):
 			for batch_num in range(len(x_list)):
-				loss = sess.run(self.op_dict['meansq'],feed_dict = {self.x_input: x_list[batch_num],self.op_dict['y_'] : y_list[batch_num], 
-					self.termination_tstep : [termination_tstep_list[batch_num]]})
+				loss = sess.run(op_list[2],feed_dict = {op_list[0] : y_list[batch_num], self.termination_tstep : termination_tstep_list[batch_num] })
 				loss_array[batch_num] = loss
-		
+
 		return sess,loss_array
 
-	def evaluate_graph(self,y,x,termination_tstep,sess):
-		"""
-		use the sessions object to run the graph for the data that is given for evaluation
-		"""
-		#use the sess object to get the predicted data
-		predicted_y = sess.run(y,feed_dict = {self.op_dict['x_'] : x, self.op_dict['termination_tstep'] : termination_tstep })
-		#pick a random batch out of the predicted
-		random_batch = np.random.rand() * BATCH_SIZE
-		#plot the random batch against the 
-		y_sample_batch = np.reshape(predicted_y[random_batch,:,:],[OUTPUT_FEATURES,DECODER_MAX_LENGTH])
-		plt.figure()
-		plt.plot(x[random_batch,:,:],label = "input")
-		plt.plot(y_sample_batch,label = "Predicted Output")
-		plt.legend()
+
+	def evaluate_graph(self,sess,op_list):
+			print np.shape(sess.run(op_list[1],feed_dict = {self.x : np.random.rand(BATCH_SIZE,INPUT_FEATURES,ENCODER_MAX_LENGTH),self.termination_tstep : [6]}))
 
 
+my_graph = variable_lstm()
+sess,op_list = my_graph.build_graph()
+my_graph.evaluate_graph(sess,op_list)
 
-state = tf.truncated_normal(shape = [BATCH_SIZE,HIDDEN_UNITS*2],stddev = 0.1)
-my_lstm = variable_lstm()
-sess = my_lstm.construct_graph(state)
-x_list = [np.random.rand(BATCH_SIZE,INPUT_FEATURES,6)] * 5
-y_list = [np.random.rand(BATCH_SIZE,OUTPUT_FEATURES,DECODER_MAX_LENGTH)] * 5
-termination_tstep_list = [3] * 5
-sess,loss_array = my_lstm.train_graph(1,x_list,y_list,termination_tstep_list,sess)
+
