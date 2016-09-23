0100c8e2be4c5fa47bb024838fa181bb57fa23a9
Shayaan Abdullah <sabdullah@berkeley.edu>
2016-09-23 01:23:19 -0700

    CLean up Code

--- variable_seq2seq.py
+++ variable_seq2seq.py
@@ -42,7 +42,8 @@ def extract_data_lstm(EPOCHS):
 	#now we know the length of x_list and y_list
 	x_list = [0] * (num_batches_in_Epoch * EPOCHS)
 	y_list = [0] * (num_batches_in_Epoch * EPOCHS)
-	termination_tstep_list = [0] * (num_batches_in_Epoch * EPOCHS)
+	batch_termination_tstep_list = [0] * (num_batches_in_Epoch * EPOCHS)
+	item_termination_tstep_list = [0] * (num_batches_in_Epoch * EPOCHS)
 
 	#inorder to make looping easier define a shape array
 	shape_state_array_first_arm = [rectangle_state_first_arm,square_state_first_arm,triangle_state_first_arm]
@@ -54,7 +55,7 @@ def extract_data_lstm(EPOCHS):
 		y_temp = np.zeros([BATCH_SIZE,OUTPUT_FEATURES,DECODER_MAX_LENGTH])
 		
 		batch_index = batch_num % num_batches_in_Epoch
-		termination_tstep = 0
+		batch_termination_tstep = 0
 		
 		for j,shape_state in enumerate(shape_state_array_first_arm):
 			for i in range(num_each_shape_per_batch):
@@ -62,7 +63,7 @@ def extract_data_lstm(EPOCHS):
 				#get the number of time steps in each input
 				_,timesteps = np.shape(shape_state[shape_index])
 				if timesteps > termination_tstep:
-					termination_tstep = timesteps
+					batch_termination_tstep = timesteps
 				x_temp[j*num_each_shape_per_batch + i,0:INPUT_FEATURES,:timesteps] = shape_state[shape_index]
 
 		for j,shape_state in enumerate(shape_state_array_second_arm):
@@ -94,9 +95,8 @@ class variable_lstm:
 		self.termination_tstep = tf.placeholder(tf.int32,shape = [1])
 		#initialize LSTM cell
 		self.lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(HIDDEN_UNITS,state_is_tuple = True)
-		#define the input of shape [Batch_size,Num of hidden units,outputs]
-		self.x = tf.placeholder(tf.float32,shape = [BATCH_SIZE,INPUT_FEATURES,ENCODER_MAX_LENGTH])
-		self.y_ = tf.placeholder(tf.float32, shape = [BATCH_SIZE,OUTPUT_FEATURES,DECODER_MAX_LENGTH]) 
+		#define an operation dictionary
+		self.op_dict = {}
 
 	def construct_decoder(self,old_state):
 		"""
@@ -110,7 +110,7 @@ class variable_lstm:
 
 		with tf.variable_scope("Decoder_RNN") as scope:
 			#initialize the decoder lstm cell
-			decoder_lstm = tf.nn.rnn_cell.BasicLSTMCell(HIDDEN_UNITS, state_is_tuple = True)
+			decoder_lstm = tf.nn.rnn_cell.BasicLSTMCell(self.hidden_units, state_is_tuple = True)
 			#initialize a list for decoder output
 			decoder_output = [0] * DECODER_MAX_LENGTH
 			#now unroll the decoder lstm plugging in the previous input at each time step
@@ -131,8 +131,8 @@ class variable_lstm:
 		#concatentate the new tstep with the
 		index = tf.concat(0,[tf.constant([0,0]),new_tstep])
 		size = tf.constant([-1,-1,1])
-		x_sliced = tf.slice(self.x,index,size)
-		x_reshape = tf.reshape(x_sliced,shape = [BATCH_SIZE,INPUT_FEATURES])
+		x_sliced = tf.slice(self.op_dict['x'],index,size)
+		x_reshape = tf.reshape(x_sliced,shape = [self.batch_size,self.input_features])
 		#now feed this into lstm
 		output,state = self.lstm_cell(x_reshape,(old_m_state,old_c_state))
 		return new_tstep,state[0],state[1]
@@ -148,8 +148,11 @@ class variable_lstm:
 		output : op_list - a list of tensors corresponding to operations that may be required in another function
 				 sess - a tensorflow object required to evaluate operations that constitute the graph
 		"""
+		#initialize placeholders
+		self.op_dict['x'] = tf.placeholder(tf.float32,shape = [self.batch_size,self.input_features,self.encoder_max_length])
+		self.op_dict['y_'] = tf.placeholder(tf.float32, shape = [self.batch_size,self.output_features,self.decoder_max_length])
 		#initialize the state tuple
-		state_tuple = self.lstm_cell.zero_state(BATCH_SIZE,tf.float32)
+		state_tuple = self.lstm_cell.zero_state(self.batch_size,tf.float32)
 		loop_var = [self.cur_tstep,state_tuple[0],state_tuple[1]]
 		#use the above to construct the while loop 
 		r = tf.while_loop(lambda tstep,old_m_state,old_c_state : self.cond(tstep,old_m_state,old_c_state),lambda tstep,old_m_state,old_c_state : self.body(tstep,old_m_state,old_c_state), loop_var)
@@ -158,29 +161,27 @@ class variable_lstm:
 		#reshape the decoder output such that it may be used to compute a loss
 		#add a fully connected layer after the decoder_output
 		#in order to this you must first reshape the decoder output
-		decoder_output2d = tf.reshape(decoder_output,shape = [BATCH_SIZE*HIDDEN_UNITS,DECODER_MAX_LENGTH])
+		decoder_output2d = tf.reshape(decoder_output,shape = [self.batch_size*self.hidden_units,self.decoder_max_length])
 		#initialize a weight matrix for the fully connected layer
-		W_fc = tf.Variable(tf.truncated_normal(shape = [BATCH_SIZE*OUTPUT_FEATURES,BATCH_SIZE*HIDDEN_UNITS], stddev = 0.1))
+		W_fc = tf.Variable(tf.truncated_normal(shape = [self.batch_size*self.output_features,self.batch_size*self.hidden_units], stddev = 0.1))
 		#Initialize a bias for the fully connected layer
-		b_fc = tf.Variable(tf.constant(0.0, shape = [DECODER_MAX_LENGTH]))
+		b_fc = tf.Variable(tf.constant(0.0, shape = [self.decoder_max_length]))
 		#now compute the output after it passes through the full connected layer
 		fc_output = tf.nn.tanh(tf.matmul(W_fc,decoder_output2d) + b_fc)
 		#reshape the output from the fully connected layer to get y
-		y = tf.reshape(fc_output, shape = [BATCH_SIZE,OUTPUT_FEATURES,DECODER_MAX_LENGTH])
+		self.op_dict['y'] = tf.reshape(fc_output, shape = [self.batch_size,self.output_features,self.decoder_max_length])
 		#compute a loss
-		meansq = tf.reduce_mean(tf.square(tf.sub(y,self.y_)))
+		self.op_dict['meansq'] = tf.reduce_mean(tf.square(self.op_dict['y'] - self.op_dict['y_']))
 		#use the loss to define a training operation 
-		train_op = tf.train.AdamOptimizer(LEARNING_RATE).minimize(meansq)
-		#define a list of operations that would be useful 
-		op_list = [self.y_,y,meansq,train_op]
+		self.op_dict['train_op'] = tf.train.AdamOptimizer(LEARNING_RATE).minimize(self.op_dict['meansq'])
 		#initialize graph
 		sess = tf.Session()
 		#initialize variables
 		sess.run(tf.initialize_all_variables())
-		return sess,op_list
+		return sess
 
 
-	def train_graph(self,sess,op_list,x_list,y_list,termination_tstep_list):
+	def train_graph(self,sess,x_list,y_list,termination_tstep_list):
 		"""
 		Tune the parameters of the graph so that it learns the right transformation
 		inputs: sess - object required in order to evaluate a graph operation.
@@ -191,15 +192,14 @@ class variable_lstm:
 		"""
 		loss_array = [0] * len(x_list)
 		for batch_num in range(len(x_list)):
-			loss,_ = sess.run([op_list[2],op_list[-1]],feed_dict = {op_list[0] : y_list[batch_num], self.termination_tstep : termination_tstep_list[batch_num], self.x : x_list[batch_num] })
+			loss,_ = sess.run([self.op_dict['meansq'],self.op_dict['train_op']],feed_dict = {self.op_dict['y'] : y_list[batch_num], self.termination_tstep : termination_tstep_list[batch_num], self.op_dict['x'] : x_list[batch_num] })
 			loss_array[batch_num] = loss
 			if batch_num % 20 == 0:
 				print batch_num,loss
-
 		return loss_array
 
 
-	def evaluate_graph(self,sess,op_list):
+	def evaluate_graph(self,sess):
 		"""
 		"""
 		#first get the data from lstm
@@ -210,7 +210,7 @@ class variable_lstm:
 		my_arm = tt.two_link_arm(80)
 		#now use the data in x to evaluate the graph
 		for batch_num,x in enumerate(x_list):
-			y_temp = sess.run(op_list[1],feed_dict = {self.x : x, self.termination_tstep : termination_tstep_list[batch_num]})
+			y_temp = sess.run(op_list[1],feed_dict = {self.op_dict['x'] : x, self.termination_tstep : termination_tstep_list[batch_num]})
 			output_list[batch_num] = y_temp
 			#initialize a grid
 			my_grid = tt.grid('output' + str(batch_num),'Output_Images_LSTM/')
