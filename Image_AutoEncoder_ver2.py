865b4d2f41dbd0e8099a8c57e66188b639a34768
Shayaan Abdullah <sabdullah@berkeley.edu>
2016-09-24 16:26:07 -0700

    Clean up directory structure and add checkpointing and visualization of weights

--- Image_AutoEncoder_ver2.py
+++ Image_AutoEncoder_ver2.py
@@ -1,24 +1,26 @@
 from __future__ import division 
 import tensorflow as tf
+from mpl_toolkits.axes_grid1 import ImageGrid
 import numpy as np
 import matplotlib.pyplot as plt
 import png
 
 
 
+
 #Globals
 
-BATCH_SIZE = 24
+BATCH_SIZE = 6
 IMG_WIDTH = 64
 PIXEL_DEPTH = 255
 CONV_KERNELS_1 = 32
 CONV_KERNELS_2 = 64
 FC_2_UNITS = 64*64*5
-
-EPOCHS = 50
+split_data = 0.75
+EPOCHS = 20
 DIRECTORY_NAME = 'Training_Images/'
-OUTPUT_DIRECTORY = 'Output_Images_ver2/'
-EVALUATION_SIZE = 200
+
+
 
 
 def extract_batch(batch_num):
@@ -58,6 +60,11 @@ class Shape_Autoencoder:
 		self.conv_kernels_3 = 32
 		self.conv_kernels_4 = 64
 		self.op_dict = {}
+
+		#intialize some directory names
+		self.checkpoint_images_directory = "Image_Checkpoints/"
+		self.output_root_directory = "Image_Autoencoder_Ver2_Outputs/"
+		self.output_image_directory = self.output_root_directory + "Output_Images/"
 	
 
 	def build_graph(self):
@@ -83,11 +90,11 @@ class Shape_Autoencoder:
 		pool1 = tf.nn.max_pool(h_conv1, ksize =[1,2,2,1],strides = [1,2,2,1],padding = 'SAME')
 	
 		#define parameters for the second convolutional layer
-		W_conv2 = tf.Variable(tf.truncated_normal([10,10,self.conv_kernels_1,self.conv_kernels_2],stddev = 0.1))
+		self.op_dict['W_conv2'] = tf.Variable(tf.truncated_normal([10,10,self.conv_kernels_1,self.conv_kernels_2],stddev = 0.1))
 		b_conv2 = tf.Variable(tf.constant(0.1,shape = [self.conv_kernels_2]))
 
 		#consider second layer
-		conv2 = tf.nn.conv2d(pool1,W_conv2,strides = [1,1,1,1],padding = 'SAME')
+		conv2 = tf.nn.conv2d(pool1,self.op_dict['W_conv2'],strides = [1,1,1,1],padding = 'SAME')
 		h_conv2 = tf.sigmoid(tf.nn.bias_add(conv2,b_conv2))
 		pool2 = tf.nn.max_pool(h_conv2, ksize = [1,2,2,1],strides = [1,1,1,1], padding = 'SAME')
 		#Reshape the output from pooling layers to pass to fully connected layers
@@ -147,21 +154,21 @@ class Shape_Autoencoder:
 		#define a learning rate with an exponential decay,a batch variable is needed in order to prevent 
 		self.op_dict['batch'] = tf.Variable(0,trainable = False)
 		self.op_dict['learning_rate'] = tf.train.exponential_decay(
-      				1.,                		# Base learning rate.
+      				1e-1,                		# Base learning rate.
       				self.op_dict['batch'],  	# Current index into the dataset.
       				100,      		# Decay step.
       				0.9,             			# Decay rate.
       				staircase=False)
 		
 		#define a training operation
-		self.op_dict['train_op'] = tf.train.MomentumOptimizer(self.op_dict['learning_rate'],1.).minimize(self.op_dict['meansq'],global_step = self.op_dict['batch'])
+		self.op_dict['train_op'] = tf.train.AdamOptimizer(self.op_dict['learning_rate']).minimize(self.op_dict['meansq'],global_step = self.op_dict['batch'])
 		sess = tf.Session()
 		sess.run(tf.initialize_all_variables())
 		
 		return sess
 
 
-	def train_graph(self,sess,test_data_index):
+	def train_graph(self,sess):
 		"""
 		Tune the weights of the graph so that you can learn the right results
 		inputs: A sessions object and an operation dictionary along with an integer specifying the end of the training data
@@ -175,6 +182,9 @@ class Shape_Autoencoder:
 		#iterate over the steps training at each step and recording the loss
 		for batch_num in range(num_batches):
 			#get the data batch by specifying the batch index as step % BATCH_SIZE
+			if batch_num % 100 == 0:
+				#evaluate the batch and save the outputs
+				self.evaluate_graph(sess,batch_num % num_batches,(batch_num + 1) % num_batches,True)
 			self.op_dict['batch'].assign(batch_num)
 			data_batch = extract_batch(batch_num)
 			feed = {self.op_dict['x'] : data_batch , self.op_dict['y_'] : data_batch}
@@ -184,17 +194,21 @@ class Shape_Autoencoder:
 			loss_array[batch_num] = loss
 		return loss_array
 
-	def evaluate_graph(self,sess,test_data_index):
+	def evaluate_graph(self,sess,start_batch_index,end_batch_index,checkpoint_boolean):
 		"""
 		Pass the testing data through the graph and save the output image for each input image
 		to the output image directory.
-		input: test_data_index is an integer specifying what the starting index of the test data is
-				a sess object is needed to evaluate the graph and the op_dict provides the nodes
+		input: start_batch_index and end_batch_index indicate start and end point for evaluations
+				checkpoint_boolean specifies if the evaluation is being used for checkpointing. 
 		output: -
 		"""
 		shape_str_array = ['Rectangle', 'Square', 'Triangle']
 		#iterate over the batches
-		for batch_index in range(EVALUATION_SIZE // self.batch_size):
+		if checkpoint_boolean:
+			output_directory = self.output_root_directory + self.checkpoint_images_directory
+		else:
+			output_directory = self.output_image_directory
+		for batch_index in range(start_batch_index,end_batch_index):
 			#call on the batch generator
 			data_batch = extract_batch(batch_index)
 			output = np.array(sess.run(self.op_dict['y'],feed_dict = {self.op_dict['x'] : data_batch}))
@@ -205,7 +219,7 @@ class Shape_Autoencoder:
 				shape_str = shape_str_array[j // (self.batch_size // 3)]
 				#once the shape name is known determine the shape index
 				shape_index = j%(self.batch_size // 3) + batch_index*(self.batch_size // 3)
-				save_name = OUTPUT_DIRECTORY + shape_str + str(shape_index) + '.png'
+				save_name = output_directory + shape_str + str(shape_index) + '.png'
 				temp = output[j,:,:,0] * PIXEL_DEPTH
 				png.from_array((temp).tolist(),'L').save(save_name)
 
@@ -213,20 +227,38 @@ class Shape_Autoencoder:
 	def save_normalized_weights(self,sess):
 		"""
 		Takes an input of weights and saves them as images so that training may be observed
-		inputs:  
+		inputs: A sessions object to evaluate the weights
 		"""
-		W_conv1 = sess.run(self.op_dict['W_conv1'])
+		W_conv1,W_conv2 = sess.run([self.op_dict['W_conv1'],self.op_dict['W_conv2']])
+		#initialize a figure to store the images
+		conv1_fig = plt.figure(1,(20.,20.))
+		#initialize an image grid
+		conv1_grid = ImageGrid(conv1_fig, 111,nrows_ncols=(self.conv_kernels_1 // 8,8),axes_pad = 0.1) 
 		for i in range(self.conv_kernels_1):
 			kernel = W_conv1[:,:,0,i]
-			kernel_normed = np.divide(kernel,np.mean(kernel))
-			plt.imsave("Kernels_Ver2/" + "kernel" + str(i) + ".png",kernel_normed)
+			kernel_normed = np.divide(kernel,np.mean(kernel)) * 255
+			conv1_grid[i].imshow(kernel_normed, cmap = "Greys_r")
+		
+		conv1_fig.savefig("Image_Autoencoder_Ver2_Outputs/Conv1_Kernels.png")
+		#perform the above for second conv layer as well
+		conv2_fig = plt.figure(1,(20.,20.))
+		conv2_grid = ImageGrid(conv2_fig, 111,nrows_ncols=(self.conv_kernels_2 // 8,8),axes_pad = 0.1)
+		for j in range(self.conv_kernels_2):
+			kernel = W_conv2[:,:,0,j]
+			kernel_normed = np.divide(kernel,np.mean(kernel)) * 255
+			conv2_grid[j].imshow(kernel_normed,cmap = "Greys_r")
+
+		conv2_fig.savefig("Image_Autoencoder_Ver2_Outputs/Conv2_Kernels.png")
 
 
 
-test_data_index = 220
+
+
+
+
 my_autoencoder = Shape_Autoencoder()
 sess = my_autoencoder.build_graph()
-loss = my_autoencoder.train_graph(sess,test_data_index)
-my_autoencoder.evaluate_graph(sess,test_data_index)
+loss = my_autoencoder.train_graph(sess)
+my_autoencoder.evaluate_graph(sess,0,int((EPOCHS * 3000) // self.batch_size),False)
 my_autoencoder.save_normalized_weights(sess)
 
