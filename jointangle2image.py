ce40ee9e40b85257ec25a56dc6b2284294ebdc95
Shayaan Abdullah <sabdullah@berkeley.edu>
2016-11-02 00:01:50 -0700

    Add dropout layer

--- jointangle2image.py
+++ jointangle2image.py
@@ -36,6 +36,7 @@ TRAIN_SIZE = 400
 ROOT_DIR = "Joints_to_Image/"
 EVAL_FREQUENCY = 200
 DISPLAY = False
+KEEP_PROB = 0.8
 
 
 ##############################LEGACY############################
@@ -298,8 +299,9 @@ encoded_image,image_encode_variable_list = encode_input_image(x_image)
 encoded_joints, joint_encoder_variable_list = encode_joints(x_joint)
 #now concatenate the two encoded vectors to get a single vector that may be decoded to an output image
 h_encoded = tf.concat(1,[encoded_image,encoded_joints])
+h_encoded_dropped = tf.nn.dropout(h_encoded,KEEP_PROB)
 #decode to get image
-y,decoder_variable_list = decode_outputs(h_encoded)
+y,decoder_variable_list = decode_outputs(h_encoded_dropped)
 
 
 #now define a loss between y and the target image
@@ -308,8 +310,8 @@ loss = -tf.reduce_mean(tf.mul(y_,tf.log(y+1e-10)) + tf.mul(1.-y_,tf.log(1.-y + 1
 opt = tf.train.AdamOptimizer(learning_rate)
 variable_names = ["W_conv1","W_conv2","W_conv3","b_conv1","b_conv2","b_conv3","W_image_fc1","b_image_fc1","W_joint_fc1","b_joint_fc1","W_joint_fc2","b_joint_fc2","W_deconv1","W_deconv2","W_deconv3","W_deconv4","W_deconv5","b_deconv1","b_deconv2","b_deconv3","b_deconv4","b_deconv5"]
 grads_and_vars = opt.compute_gradients(loss, image_encode_variable_list + joint_encoder_variable_list + decoder_variable_list)
-#summary_nodes = [tf.histogram_summary(variable_names[i],gv[0]) for i,gv in enumerate(grads_and_vars)]
-#merged = tf.merge_all_summaries()
+summary_nodes = [tf.histogram_summary(variable_names[i],gv[0]) for i,gv in enumerate(grads_and_vars)]
+merged = tf.merge_all_summaries()
 train_op = opt.apply_gradients(grads_and_vars)
 
 
@@ -339,16 +341,16 @@ def train_graph():
 				feed_dict = { x_joint: joint_batch, x_image : input_image_batch, y_ : target_image_batch}
 
 				#run the graph
-				_, l= sess.run(
-					[train_op,loss],
+				_, l, merged_summary= sess.run(
+					[train_op,loss,merged],
 					feed_dict=feed_dict)
 				training_loss_array[step] = l
 
 				if step % 20 == 0:
+					train_writer.add_summary(merged_summary,step)
 					print step,l
 				if step % EVAL_FREQUENCY == 0:
 					predictions,test_loss_array = eval_in_batches(sess)
-					#train_writer.add_summary(merged_summary,step)
 					print "Test Loss is " + str(np.mean(test_loss_array))
 			predictions,test_loss_array = eval_in_batches(sess)
 		return predictions,training_loss_array,test_loss_array
