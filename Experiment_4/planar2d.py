b1e34fadec566ce5b32a3db6537509c13a97c96c
Shayaan Abdullah <sabdullah@berkeley.edu>
2017-02-11 19:06:16 -0800

    Add reset

--- Experiment_4/planar2d.py
+++ Experiment_4/planar2d.py
@@ -6,53 +6,94 @@ from rllab.spaces.discrete import Discrete
 from rllab.spaces.box import Box
 
 
-class Planar2D(Env):
+class env_2DOF_arm(Env):
     """
-    A simple env whose output is a value `X` the first time step, followed by a
-    fixed number of zeros.
-
-    The reward is 1 for all steps if the agent outputs zero except at the end,
-    where the agent gets a large reward if `X` is returned correctly.
-
-    Both the actions and observations are represented as one-hot vectors.
-    There are `n` different values that `X` can take on (excluding 0),
-    so the one-hot vector's dimension is n+1.
+    Environment for a planar arm which takes actions via a delta theta
+    and where its observations are a visual input of the planar surface
+    The arm is reward whenever it reaches within an epsilon of the taret surface
     """
 
-    def __init__(self, n=4, num_steps=100, reward_for_remembering=1000):
+
+    def __init__(self,num_steps = 100,epsilon = 10,theta_i = np.array([0.,np.pi/2]),link_length = 50,target_loc = (np.random.randint(67,125),np.random.randint(67,125))):
         """
-        :param n: Number of different values that could be returned
-        :param num_steps: How many steps the agent needs to remember.
-        :param reward_for_remembering: The reward for remembering the number.
+        theta_i of shape [2] np array
         """
-        super().__init__()
+        self.prev_theta = theta_i
+        self.link_length = link_length
+        self.target = target_loc
+        self.sp = tt.shape_maker()
+        self.init_theta = theta_i
+        self.cur_theta = theta_i
+        self.cur_image = self.render_image()
+        self.epsilon = 4
         self.num_steps = num_steps
-        self.n = n
-        self._action_space = Discrete(self.n + 1)
-        self._observation_space = Discrete(self.n + 1)
-        self._t = 1
+        self._action_space = 
+        self._observation_space =
 
-        self._target = None
-        self._next_obs = None
 
     def step(self, action):
-        # flatten = to one hot...not sure why it was given that name.
-        observation = self._get_next_observation()
-        self._next_obs = 0
+        """
+        Run one timestep of the environment's dynamics. When end of episode
+        is reached, reset() should be called to reset the environment's internal state.
+        Input
+        -----
+        action : an action provided by the environment
+        Outputs
+        -------
+        (observation, reward, done, info)
+        observation : visualization of the 2DOF arm over the planar surface
+        reward [Float] : Add a reward when the arm end effector is within the target
+        done : a boolean, indicating whether the episode has ended
+        info : a dictionary containing other diagnostic information from the previous action
+        """
+                """
+        action is a delta theta numpy array of shape [2]
+        """
+        self.cur_theta = self.prev_theta + action
+        self.cur_image = self.render_image()
+        self.prev_theta = self.cur_theta
+        #in addition compute the reward from the previous action
+        #compare the end effector position to the target position and determine whether it is within epsilon of the target
+        if abs(self.end_effector[0] - self.target[0]) < self.epsilon and abs(self.end_effector[1] - self.target[1]) < self.epsilon:
+            reward = 1.
+        else
+            reward = 0.
 
-        done = self._t == self.num_steps
-        self._t += 1
+        if abs(self.end_effector[0] - self.target[0]) < 3 and abs(self.end_effector[1] - self.target[1]) < 3:
+            done = True
+        else
+            done = False
+        
+        #let info record the joint angle state at the current timestep
+        info = {"Joint State" : self.cur_theta}
 
-        if done:
-            reward = self._reward_for_remembering * int(
-                self._observation_space.unflatten(action) == self._target
-            )
-        else:
-            reward = int(
-                self._observation_space.unflatten(action) == 0
-            )
-        info = {'target': self.n}
-        return observation, reward, done, info
+        return self.cur_image,reward,done,info
+
+    def render_image(self):
+        theta_1 = self.cur_theta[0]
+        theta_2 = self.cur_theta[1]
+        #specify the anchor point
+        start_x = 63
+        start_y = 63
+        start_point = (start_x,start_y)
+        #get the link end positions
+        x_link_1 = np.round(np.cos(theta_1)*self.link_length + start_x)
+        y_link_1 = np.round(np.sin(theta_1)*self.link_length + start_y)
+        x_link_2 = np.round(x_link_1 + np.cos(theta_1 + theta_2)*self.link_length)
+        y_link_2 = np.round(y_link_1 + np.sin(theta_1 + theta_2)*self.link_length)
+        link1_end_point = (int(x_link_1),int(y_link_1))
+        link2_end_point = (int(x_link_2),int(y_link_2))
+        self.end_effector = link2_end_point
+        pos_list = self.sp.draw_line(start_point,link1_end_point,0.1,0.1) + self.sp.draw_line(link1_end_point,link2_end_point,0.1,0.1) + [self.target]
+        #now get the extended point list in order to thicken the lines
+        additional_points = self.sp.get_points_to_increase_line_thickness(pos_list)
+        #now initialize a grid in order to save the correct images
+        temp_grid = tt.grid(grid_size = (128,128))
+        #draw the the points
+        temp_grid.draw_figure(pos_list)
+        #thicken the lines
+        cur_image = temp_grid.draw_figure(additional_points)
+        return cur_image
 
     @property
     def action_space(self):
@@ -63,41 +104,48 @@ class Planar2D(Env):
         return self.num_steps
 
     def reset(self):
-        self._target = randint(1, self.n)
-        self._next_obs = self._target
-        self._t = 1
-        return self._get_next_observation()
+        """
+        Resets the state of the environment, returning an initial observation.
+        #this would require setting the observation to the 
+        -------
+        observation : the initial observation of the space. (Initial reward is assumed to be 0.)
+        """
+        #Reset the theta to the initial theta
+        self.cur_theta = self.init_theta
+        #render the grid using this theta and
+        return self.render_image()
 
-    def _get_next_observation(self):
-        return self._observation_space.flatten(self._next_obs)
+
+    # def _get_next_observation(self):
+    #     return self._observation_space.flatten(self._next_obs)
 
     @property
     def observation_space(self):
         return self._observation_space
 
-    def get_batch(self, batch_size):
-        targets = np.random.randint(
-            low=1,
-            high=self.n,
-            size=batch_size,
-        )
-        onehot_targets = special.to_onehot_n(targets, self.feature_dim)
-        X = np.zeros((batch_size, self.sequence_length, self.feature_dim))
-        X[:, :, 0] = 1  # make the target 0
-        X[:, 0, :] = onehot_targets
-        Y = np.zeros((batch_size, self.sequence_length, self.target_dim))
-        Y[:, :, 0] = 1  # make the target 0
-        Y[:, -1, :] = onehot_targets
-        return X, Y
+    # def get_batch(self, batch_size):
+    #     targets = np.random.randint(
+    #         low=1,
+    #         high=self.n,
+    #         size=batch_size,
+    #     )
+    #     onehot_targets = special.to_onehot_n(targets, self.feature_dim)
+    #     X = np.zeros((batch_size, self.sequence_length, self.feature_dim))
+    #     X[:, :, 0] = 1  # make the target 0
+    #     X[:, 0, :] = onehot_targets
+    #     Y = np.zeros((batch_size, self.sequence_length, self.target_dim))
+    #     Y[:, :, 0] = 1  # make the target 0
+    #     Y[:, -1, :] = onehot_targets
+    #     return X, Y
 
-    @property
-    def feature_dim(self):
-        return self.n + 1
+    # @property
+    # def feature_dim(self):
+    #     return self.n + 1
 
-    @property
-    def target_dim(self):
-        return self.n + 1
+    # @property
+    # def target_dim(self):
+    #     return self.n + 1
 
-    @property
-    def sequence_length(self):
-        return self.horizon
+    # @property
+    # def sequence_length(self):
+    #     return self.horizon
