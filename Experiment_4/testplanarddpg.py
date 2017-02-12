d54119394cfbc8438ab35bd61ca29800b25a3a09
mudigonda <mayur.mudigonda@gmail.com>
2017-02-11 18:09:47 -0800

    Pushing template for the planar reaching class

---
+++ Experiment_4/testplanarddpg.py
@@ -0,0 +1,38 @@
+import tensorflow as tf
+
+from algos.ddpg import DDPG
+from policies.nn_policy import FeedForwardPolicy
+from qfunctions.nn_qfunction import FeedForwardCritic
+from rllab.exploration_strategies.ou_strategy import OUStrategy
+from rllab.misc.instrument import run_experiment_lite, stub
+from planar2d import Planar2D
+
+
+def run_task(*_):
+    env = Planar2D()
+    es = OUStrategy(env_spec=env.spec)
+    qf = FeedForwardCritic(
+        name_or_scope="critic",
+        env_spec=env.spec,
+    )
+    policy = FeedForwardPolicy(
+        name_or_scope="actor",
+        env_spec=env.spec,
+    )
+    algorithm = DDPG(
+        env,
+        es,
+        policy,
+        qf,
+    )
+    algorithm.train()
+
+
+if __name__ == "__main__":
+    run_experiment_lite(
+        run_task,
+        n_parallel=1,
+        snapshot_mode="last",
+        exp_prefix="ddpg-planar2D",
+        seed=2,
+    )
