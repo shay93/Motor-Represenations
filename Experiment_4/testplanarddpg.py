bafd4dbc7f613913e2cbe21746056d51223a2b5b
Shayaan <sabdullah@berkeley.edu>
2017-02-13 15:11:38 -0800

    latest running version

--- Experiment_4/testplanarddpg.py
+++ Experiment_4/testplanarddpg.py
@@ -1,14 +1,14 @@
 import tensorflow as tf
 
 from algos.ddpg import DDPG
-from policies.nn_policy import FeedForwardPolicy
+from base_nn_policy import Conv_FeedForwardPolicy
 from qfunctions.nn_qfunction import FeedForwardCritic
 from rllab.exploration_strategies.ou_strategy import OUStrategy
 from rllab.misc.instrument import run_experiment_lite, stub
 from planar2d import  env_2DOF_arm
 from planarspace import PlanarSpace
 from sandbox.rocky.tf.envs.base import TfEnv
-
+import IPython
 
 def run_task(*_):
     env = env_2DOF_arm()
@@ -17,7 +17,7 @@ def run_task(*_):
         name_or_scope="critic",
         env_spec=env.spec,
     )
-    policy = FeedForwardPolicy(
+    policy = Conv_FeedForwardPolicy(
         name_or_scope="actor",
         env_spec=env.spec,
     )
@@ -26,8 +26,13 @@ def run_task(*_):
         es,
         policy,
         qf,
+        batch_size = 500,
+        n_epochs = 1000,
+        min_pool_size = 1000,
+        replay_pool_size = 200000,
     )
     algorithm.train()
+
 
 
 if __name__ == "__main__":
@@ -37,4 +42,4 @@ if __name__ == "__main__":
         snapshot_mode="last",
         exp_prefix="ddpg-planar2D",
         seed=2,
-    )
+  ) 
