0d84c126863967fa6221e5114a2a833bd09b221e
Shayaan Abdullah <sabdullah@berkeley.edu>
2017-02-19 00:21:01 -0800

    Modify sim policy so that it can save images it evaluates

--- Experiment_4/sim_policy.py
+++ Experiment_4/sim_policy.py
@@ -6,9 +6,11 @@ import uuid
 import tensorflow as tf
 from planar2d import  env_2DOF_arm
 import json
+import matplotlib.pyplot as plt
+import os
 
 filename = str(uuid.uuid4())
-
+root_dir = "Eval_Sequences/"
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
@@ -18,6 +20,10 @@ if __name__ == "__main__":
                         help='Max length of rollout')
     parser.add_argument('--speedup', type=float, default=1,
                         help='Speedup')
+    
+    parser.add_argument('--num_sequences',type=int,default=1000,
+                        help='Number of sequences to evaluate')
+
     args = parser.parse_args()
 
     policy = None
@@ -37,7 +43,7 @@ if __name__ == "__main__":
         env = data['env']
         rollout_lst_dict = []
         idx = 0
-        while True:
+        while idx < args.num_sequences:
             print("Inside the loop")
             try:
                 print("Trying to rollout")
@@ -51,5 +57,18 @@ if __name__ == "__main__":
                               "argument 'close'"):
                     raise e
             idx +=1
-            if np.mod(idx,10) == 0:
-                import IPython; IPython.embed()
+
+
+    #now run through all the sequences and plot them
+    for j in range(args.num_sequences):
+        seq_directory = root_dir + "sequence_" + str(j) + "/"
+        #first create a directory
+        if not(os.path.exists(seq_directory)):
+            os.makedirs(seq_directory)
+        seq_length = np.shape(rollout_lst_dict[j]['observations'])[0]
+        for i in range(seq_length):
+            plt.imsave(seq_directory + "timestep" + str(i),
+                rollout_lst_dict[j][i,:].reshape((64,64)),
+                cmap = "Greys_r")
+
+
