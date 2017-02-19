5068230a55562ccaa1e5489f903d4b06cb349c93
Shayaan <sabdullah@berkeley.edu>
2017-02-19 00:23:37 -0800

    Change default value for max path length

--- Experiment_4/sim_policy.py
+++ Experiment_4/sim_policy.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('file', type=str,
                         help='path to the snapshot file')
-    parser.add_argument('--max_path_length', type=int, default=1000,
+    parser.add_argument('--max_path_length', type=int, default=100,
                         help='Max length of rollout')
     parser.add_argument('--speedup', type=float, default=1,
                         help='Speedup')
