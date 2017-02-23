f8e7958d1995891e904538043c0c6672631a9300
Shayaan Abdullah <sabdullah@berkeley.edu>
2017-02-22 23:43:57 -0800

    Add new Billiards Environment and correct bug in training tools

---
+++ Experiment_4/Billiards/billiards_2D.py
@@ -0,0 +1,144 @@
+import numpy as np
+from rllab.envs.base import Env
+from rllab.spaces.box import Box
+import os
+import sys
+from scipy.misc import imresize
+import matplotlib.pyplot as plt
+from planarspace import PlanarSpace
+
+parent_dir = os.path.dirname(os.getcwd())
+
+import training_tools as tt
+import IPython
+
+class Billiards_2D(Env):
+    """
+    2D Billiards environment
+    """
+
+
+    def __init__(self,num_steps = 100,epsilon = 15, box_width = 5,step_size = 3):
+        """
+        theta_i of shape [2] np array
+        """
+        #specify the box width for the actor and the target
+        self.box_width = box_width
+        #initialize the centre of the target box on the 128 by 128 grid by sampling from a uniform distribution
+        self.target = [tuple(np.round(np.random.uniform(10,54,size = 2)))]
+        #initialize a helper function to draw the boxes on the grid
+        self.sp = tt.shape_maker()
+        #initialize the actors location as well by sampling 
+        self.actor = self.get_actor_loc()
+        #using the actor location and the target location render an observation image
+        self.cur_obs_image = self.render_image()
+        #if centre of the actor box is within epsilon of the target give a reward
+        self.epsilon = epsilon
+        #indicate the horizon
+        self.num_steps = num_steps
+        #indicate the number of pixels to jump over when a single action is taken
+        self.step_size = step_size
+        #specify the action and observation space
+        self._action_space = Box(0,1,(4))
+        self._observation_space = PlanarSpace()
+
+    def get_actor_loc(self):
+        """
+        Sample from a uniform distribution to get actors location in grid
+        Call on reset and initialization return a list with a single tuple as an element
+        """
+        #sample to get a tentative, initial actor location
+        actor_loc = [tuple(np.round(np.random.uniform(10,54,size = 2)))]
+        #check if the initial actor location overlaps with the target location if so get
+        #get the actor location again
+        if self.check_overlap: #there is overlap
+            return self.get_actor_loc()
+        else:
+            return actor_loc
+
+    def check_overlap(self):
+        """
+        Check if the actor and target boxes overlap
+        Return a boolean indicating overlap or not
+        """
+        if abs(self.target[0][0] - self.actor[0][0]) < self.box_width or abs(self.target[0][1] - self.actor[0][1]) < self.box_width:
+            return True
+
+        return False
+
+    def step(self, action):
+        """
+        Run one timestep of the environment's dynamics. When end of episode
+        is reached, reset() should be called to reset the environment's internal state.
+        Input
+        -----
+        action : an action provided by the environment 1d array of size 4 [left,right,up,down]
+        -------
+        (observation, reward, done, info)
+        reward [Float] : Add a reward based on inverse of distance
+        done : a boolean, indicating whether the episode has ended
+        info : a dictionary containing other diagnostic information from the previous action
+        """
+        #take the max of the action to determine should move left,right,up,down
+        
+        amax = np.argmax(action)
+        #update the target location center based on the output
+        if amax == 0: #go left, decrement column by step
+            self.actor = [(self.actor[0][0],self.actor[0][1] - self.step_size)]
+        elif amax == 1: # go right, increment column by step
+            self.actor = [(self.actor[0][0],self.actor[0][1] + self.step_size)]
+        elif amax == 2: # go up, decrement row by step
+            self.actor = [(self.actor[0][0] - self.step_size,self.actor[0][1])]
+        elif amax == 3: # go down, increment row by step
+            self.actor = [(self.actor[0][0] + self.step_size,self.actor[0][1])]
+        
+        #get the current observation image after the step has been taken
+        self.cur_obs_image = self.render_image()
+
+        #give a reward if the actor is within
+        if abs(self.actor[0][0] - self.target[0][0]) < self.epsilon and abs(self.actor[0][1] - self.target[0][1]) < self.epsilon:
+            reward = 1.
+        else:
+            reward = 0.
+
+        #terminate the episode if the target is reached
+        done = self.check_overlap()
+        
+        return self.cur_obs_image,reward,done,None
+
+    def render_image(self):
+        #initialize a grid to draw the environment
+        temp_grid = tt.grid(grid_size = (64,64))
+        #draw the actor box in white
+        temp_grid.draw_figure(self.sp.get_points_to_increase_line_thickness(self.actor,width = self.box_width))
+        #draw the target box in grey
+        cur_image = temp_grid.draw_figure(self.sp.get_points_to_increase_line_thickness(self.target,width = self.box_width), pixel_value = 125.)
+        #renormalize observation image
+        cur_image = cur_image / 255.
+        return cur_image.flatten() 
+
+    @property
+    def action_space(self):
+        return self._action_space
+
+    @property
+    def horizon(self):
+        return self.num_steps
+
+    def reset(self):
+        """
+        Resets the state of the environment, returning an initial observation.
+        #this would require setting the observation to the 
+        -------
+        observation : the initial observation of the space. (Initial reward is assumed to be 0.)
+        """
+        print("env reset")
+        #Reset the target location by sampling 
+        self.target = [tuple(np.round(np.random.uniform(10,54,size = 2)))]
+        #Reset the actor location
+        self.actor = self.get_actor_loc()
+        return self.render_image()
+
+    @property
+    def observation_space(self):
+        return self._observation_space
---
+++ Experiment_4/Billiards/billiards_qfunction.py
@@ -0,0 +1,169 @@
+import tensorflow as tf
+
+from core.tf_util import he_uniform_initializer, mlp, linear
+from predictors.state_action_network import StateActionNetwork
+from rllab.core.serializable import Serializable
+import IPython
+
+class NNQFunction(StateActionNetwork):
+    def __init__(
+            self,
+            name_or_scope,
+            **kwargs
+    ):
+        self.setup_serialization(locals())
+        super().__init__(name_or_scope=name_or_scope, output_dim=1, **kwargs)
+
+class FeedForwardCritic(NNQFunction):
+    def __init__(
+            self,
+            name_or_scope,
+            hidden_W_init=None,
+            hidden_b_init=None,
+            output_W_init=None,
+            output_b_init=None,
+            embedded_hidden_sizes=(100,),
+            observation_hidden_sizes=(100,),
+            hidden_nonlinearity=tf.nn.relu,
+            **kwargs
+    ):
+        self.setup_serialization(locals())
+        self.hidden_W_init = hidden_W_init or he_uniform_initializer()
+        self.hidden_b_init = hidden_b_init or tf.constant_initializer(0.)
+        self.output_W_init = output_W_init or tf.random_uniform_initializer(
+            -3e-3, 3e-3)
+        self.output_b_init = output_b_init or tf.random_uniform_initializer(
+            -3e-3, 3e-3)
+        self.embedded_hidden_sizes = embedded_hidden_sizes
+        self.observation_hidden_sizes = observation_hidden_sizes
+        self.hidden_nonlinearity = hidden_nonlinearity
+        super().__init__(name_or_scope=name_or_scope, **kwargs)
+
+    def _create_network(self, observation_input, action_input):
+        with tf.variable_scope("observation_mlp") as _:
+            observation_output = mlp(observation_input,
+                                     self.observation_dim,
+                                     self.observation_hidden_sizes,
+                                     self.hidden_nonlinearity,
+                                     W_initializer=self.hidden_W_init,
+                                     b_initializer=self.hidden_b_init,
+                                     )
+        embedded = tf.concat(1, [observation_output, action_input])
+        embedded_dim = self.action_dim + self.observation_hidden_sizes[-1]
+        with tf.variable_scope("fusion_mlp") as _:
+            fused_output = mlp(embedded,
+                               embedded_dim,
+                               self.embedded_hidden_sizes,
+                               self.hidden_nonlinearity,
+                               W_initializer=self.hidden_W_init,
+                               b_initializer=self.hidden_b_init,
+                               )
+
+        with tf.variable_scope("output_linear") as _:
+            return linear(fused_output,
+                          self.embedded_hidden_sizes[-1],
+                          1,
+                          W_initializer=self.output_W_init,
+                          b_initializer=self.output_b_init,
+                          )
+
+
+class Conv_FeedForwardCritic(NNQFunction):
+    """
+    Pass observation through conv layers to obtain observation output of shape
+    [Batch,x] the action output is of shape [None,2], so concatenate
+    the tensors along dimension 1 to obtain the embedded vector
+    which may be passed to a few fuly connected layers in order to output a Q value of dimension None,1
+    """
+    def __init__(self,name_or_scope, 
+                 **kwargs):
+        self.name = str(name_or_scope)
+        self.setup_serialization(locals())
+        
+        with tf.variable_scope(name_or_scope) as scope:
+            try:
+                self.W_conv1 = tf.get_variable("W_conv1",[5,5,1,32],tf.float32,tf.random_uniform_initializer(-3e-3,3e-3))
+                self.b_conv1 = tf.get_variable("b_conv1",[32],tf.float32,tf.random_uniform_initializer(-3e-3,3e-3))
+                
+                self.W_conv2 = tf.get_variable("W_conv2",[5,5,32,32],tf.float32,tf.random_uniform_initializer(-3e-3,3e-3))
+                self.b_conv2 = tf.get_variable("b_conv2",[32],tf.float32,tf.random_uniform_initializer(-3e-3,3e-3))
+                
+                self.W_conv3 = tf.get_variable("W_conv3",[3,3,32,32],tf.float32,tf.random_uniform_initializer(-3e-3,3e-3))
+                self.b_conv3 = tf.get_variable("b_conv3",[32],tf.float32,tf.random_uniform_initializer(-3e-3,3e-3))
+                
+                self.W_fc_obs = tf.get_variable("W_fc_obs",[9*32,100],tf.float32,tf.random_uniform_initializer(-3e-3,3e-3))
+                self.b_fc_obs = tf.get_variable("b_fc_obs",[100],tf.float32,tf.random_uniform_initializer(-3e-3,3e-3))
+            
+                self.W_fc_encode_action = tf.get_variable("W_fc_action",[4,100],tf.float32,tf.random_uniform_initializer(-3e-3,3e-3))
+                self.b_fc_encode_action = tf.get_variable("b_fc_action",[100], tf.float32,tf.random_uniform_initializer(-3e-3,3e-3))
+
+                self.W_fc_embed_1 = tf.get_variable("W_fc_embed_1",[200,200],tf.float32,tf.random_uniform_initializer(-3e-3,3e-3))
+                self.b_fc_embed_1 = tf.get_variable("b_fc_embed_1",[200],tf.float32,tf.random_uniform_initializer(-3e-3,3e-3))
+                
+                self.W_fc_embed_2 = tf.get_variable("W_fc_embed_2",[200,1],tf.float32,tf.random_uniform_initializer(-3e-3,3e-3))
+                self.b_fc_embed_2 = tf.get_variable("b_fc_embed_2",[1],tf.float32,tf.random_uniform_initializer(-3e-3,3e-3))
+
+            except:
+                scope.reuse_variables()                
+                self.W_conv1 = tf.get_variable("W_conv1")
+                self.b_conv1 = tf.get_variable("b_conv1")
+                
+                self.W_conv2 = tf.get_variable("W_conv2")
+                self.b_conv2 = tf.get_variable("b_conv2")
+                
+                self.W_conv3 = tf.get_variable("W_conv3")
+                self.b_conv3 = tf.get_variable("b_conv3")
+                
+                #now initialize the variables for the fc layers
+                self.W_fc_obs = tf.get_variable("W_fc_obs")
+                self.b_fc_obs = tf.get_variable("b_fc_obs")
+
+                self.W_fc_encode_action = tf.get_variable("W_fc_action")
+                self.b_fc_encode_action = tf.get_variable("b_fc_action")
+        
+                self.W_fc_embed_1 = tf.get_variable("W_fc_embed_1")
+                self.b_fc_embed_1 = tf.get_variable("b_fc_embed_1")
+                
+                self.W_fc_embed_2 = tf.get_variable("W_fc_embed_2")
+                self.b_fc_embed_2 = tf.get_variable("b_fc_embed_2")
+
+        super(Conv_FeedForwardCritic,self).__init__(name_or_scope=name_or_scope, **kwargs)
+
+    def _create_network(self,observation_input,action_input):
+        #IPython.embed()
+        #the observation input is provided as a flattened tensor so reshape it
+        x = tf.expand_dims(tf.reshape(observation_input,shape = [-1,64,64]),-1)
+        conv1 = tf.nn.conv2d(x,self.W_conv1,strides = [1,3,3,1],padding = "SAME")
+        h_1 = tf.nn.tanh(tf.nn.bias_add(conv1,self.b_conv1))
+
+        conv2 = tf.nn.conv2d(h_1,self.W_conv2,strides = [1,3,3,1],padding = "SAME")
+        h_2 = tf.nn.tanh(tf.nn.bias_add(conv2,self.b_conv2))
+
+        conv3 = tf.nn.conv2d(h_2,self.W_conv3,strides = [1,3,3,1],padding = "SAME")
+        h_3 = tf.nn.tanh(tf.nn.bias_add(conv3,self.b_conv3))
+
+        h_3_flattened = tf.reshape(h_3,shape = [-1,9*32])
+        #finally pass through fc layer with tanh non linearity
+        observation_output = tf.nn.tanh(tf.matmul(h_3_flattened,self.W_fc_obs) + self.b_fc_obs)
+
+        #encode the action to a 200 length vector
+        action_encoded = tf.nn.tanh(tf.matmul(action_input,self.W_fc_encode_action) + self.b_fc_encode_action)
+
+        #once action has been encoded concatenate it with the observation 
+        embed = tf.concat(1,[observation_output,action_encoded])
+
+        #pass embed along the two defined fc layers
+        h_embed_1 = tf.nn.tanh(tf.matmul(embed,self.W_fc_embed_1) + self.b_fc_embed_1)
+        q_value = (tf.matmul(h_embed_1,self.W_fc_embed_2) + self.b_fc_embed_2)
+        return q_value
+
+    def get_params_internal(self, **tags):
+        #IPython.embed()
+        if "target" in self.name:
+             return [v for v in tf.all_variables() if self.name[:-1] in v.name.split("/")[0] and not("Adam" in v.name.split("/")[-1])]
+        else:
+             return [v for v in tf.all_variables() if self.name == v.name.split("/")[0] and not("Adam" in v.name.split("/")[-1])]
+    
+
+
+
