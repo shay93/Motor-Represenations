c2735ef9f6b75fe72ad9d42c8a01155723b9803a
Shayaan <sabdullah@berkeley.edu>
2017-03-25 13:28:14 -0700

    Debugged version but still have problem with kinematics

--- Data_Generation_Scripts/generate_arm_trajectories.py
+++ Data_Generation_Scripts/generate_arm_trajectories.py
@@ -1,4 +1,4 @@
-from __future__ import division,print
+
 import numpy as np
 import os
 import sys
@@ -6,6 +6,7 @@ parent_dir = os.path.dirname(os.getcwd())
 sys.path.append(parent_dir)
 import training_tools as tt
 import pickle
+import IPython
 
 def inverse_kinematics_2DOF(end_effector,link_length):
     """ Args: Effector_Position - An ndarray consisting of the desired
@@ -16,15 +17,17 @@ def inverse_kinematics_2DOF(end_effector,link_length):
     end_effector_x = end_effector[...,0]
     end_effector_y = end_effector[...,1]
     #find the angle betweeen the first and second links
-    theta_2 = np.arccos((end_effector_x**2 + \
-    end_effector_y**2 - 2*(link_length**2))/(2*(link_length**2)))
+    theta_2 = np.arccos(np.divide(end_effector_x**2 + end_effector_y**2 - 2*(link_length**2),2*(link_length**2)))
     k1 = link_length*(1 + np.cos(theta_2))
     k2 = link_length*np.sin(theta_2)
     #find the angle between first link and x axis
     theta_1 = np.arctan(end_effector_y/end_effector_x) - np.arctan(k2/k1)
     states_2DOF = np.concatenate((theta_1[...,np.newaxis], \
                             theta_2[...,np.newaxis]),axis = -1)
-    assert (np.all(forward_kinematics(states_2DOF,link_length) == end_effector)),\
+    print(end_effector[0,:5,:])
+    print(forward_kinematics(states_2DOF,link_length)[0,:5,:])
+    IPython.embed()
+    assert (np.all(np.round(forward_kinematics(states_2DOF,link_length)) == np.round(end_effector))),\
     "Inverse and Forward Kinematics dont line up"
     return states_2DOF
     
@@ -33,13 +36,15 @@ def forward_kinematics(joint_angle_state,link_length):
 	use the joint angle information to get an end effector position 
 	"""
 	#find the number of degrees of freedom of the arm
-	n_dof = joint_angle_state[-1]
+	n_dof = joint_angle_state.shape[-1]
 	#initialize the xpos and ypos
-	xpos = np.ndarray(shape = joint_angle_state.shape[:-1] + (1,))
-	ypos = np.ndarray(shape = joint_angle_state.shape[:-1] + (1,))
+	xpos = np.zeros(shape = joint_angle_state.shape[:-1] + (1,))
+	ypos = np.zeros(shape = joint_angle_state.shape[:-1] + (1,))
 	for i in range(1,n_dof+1):
-		xpos += link_length*np.cos(np.sum(joint_angle_state[...,:i]))
-		ypos += link_length*np.sin(np.sum(joint_angle_state[...,:i]))
+		print(i)
+		#IPython.embed()
+		xpos += link_length*np.cos(np.expand_dims(np.sum(joint_angle_state[...,:i],axis = -1),-1))
+		ypos += link_length*np.sin(np.expand_dims(np.sum(joint_angle_state[...,:i],axis = -1),-1))
 	return np.concatenate((xpos,ypos),axis = -1)
 
 #specify the num_sequences for which to generate data for
@@ -61,18 +66,18 @@ states_3DOF = np.mod(np.cumsum(np.concatenate(
 next_states_3DOF = np.mod(states_3DOF + actions_3DOF,2.)
     
 #as a sanity check the dimensions of states_3DOF
-print(states_3DOF.shape)
+#print(states_3DOF.shape)
 #check what the min and max of the 3DOF states are
 print(np.max(states_3DOF))
 print(np.min(states_3DOF))
 #now compute the end effector position of the 3DOF arm
 end_effector = forward_kinematics(states_3DOF,30)
 #use the end effector position to get the state for the 2DOF arm
-states_2DOF = inverse_kinematics_2DOF(end_effector,40)
+states_2DOF = inverse_kinematics_2DOF(end_effector,50)
 #get the next end_effector position at each timestep
 next_end_effector = forward_kinematics(next_states_3DOF,30)
 #get the next states for 2DOF
-next_states_2DOF = inverse_kinematics_2DOF(next_end_effector,40)
+next_states_2DOF = inverse_kinematics_2DOF(next_end_effector,50)
 #use the next states and current states for the 2DOF to get the 2DOF actions
 actions_2DOF = next_states_2DOF - states_2DOF
 
